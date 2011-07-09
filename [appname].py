84c2659484982bc03a7e41388453e98ac80d591d
Joe Esposito <joe@joeyespo.com>
2011-07-09 13:28:03 -0400

    Fixed up the 'static' jinja function to use the app's static_url_path and static_folder values.

--- [appname].py
+++ [appname].py
@@ -12,12 +12,8 @@ from flask import Flask, render_template, url_for
 
 def static(filename):
     """Provides the 'static' function that also appends the file's timestamp to the URL, usable in a template."""
-    url = url_for('.static', filename=filename)
-    fname = os.path.join(os.path.join(app.root_path, 'static'), filename)
-    st = int(os.path.getmtime(fname))
-    # time.strftime('%Y-%m-%d %H:%M', st)
-    url += '?' + str(st)
-    return url
+    timestamp = os.path.getmtime(os.path.join(app.static_folder, filename))
+    return "%s/%s?%s" % (app.static_url_path, filename, timestamp)
 
 # Flask application
 app = Flask(__name__)
